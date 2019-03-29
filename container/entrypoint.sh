5ce79c71d65dc0df38ac2386a7f1075b5ff44b82
lcavajani <lcavajani@suse.com>
2019-03-29 18:41:02 +0100

    Update containers
    Signed-off-by: lcavajani <lcavajani@suse.com>

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
+  if [ ! -z "$CONF_FILE" ]; then
+    PARAMS="--conf-file $CONF_FILE"
+  fi
+
   if [ ! -z "$OSCRC_FILE" ]; then
     # osc needs to 'chmod 0600' the oscrc file
     # but this is an issue when mounting the file
     # in a container
-    OSCRC_FILE_NEW="$OSCRC_FILE".new
+    OSCRC_FILE_NEW="/tmp/$(basename $OSCRC_FILE).new"
     cp -v "$OSCRC_FILE" "$OSCRC_FILE_NEW"
-    PARAMS="--oscrc-file $OSCRC_FILE_NEW"
-  fi
-  
-  if [ ! -z "$CONF_FILE" ]; then
-    PARAMS="$PARAMS --conf-file $CONF_FILE"
+    PARAMS="$PARAMS --oscrc-file $OSCRC_FILE_NEW"
   fi
   
   if [ "$SHOW_OSC_COMMANDS" = "true" ]; then
